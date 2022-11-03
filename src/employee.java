6d01fb9ea45299129519f58e4f7bc11ef62f50ae
HP <mdzaid1299@gmail.com>
2022-11-04 02:58:20 +0530

    ini

--- src/employee.java
+++ src/employee.java
@@ -14,16 +14,5 @@ public class employee {
         this.age = age;
     }
 
-    class Emplo {
-        public static void main(String[] args) {
-            employee e1 = new employee(101, "Ayush", 56000, 65);
-            employee e2 = new employee(101, "Monish vysa", 56000, 65);
-            employee e3 = new employee(101, "abhi", 56000, 65);
-            ArrayList<employee> List = new ArrayList<>();
-            List.add(e1);
-            List.add(e2);
-            List.add(e3);
-            List.stream().filter(date-> date.age>26).forEach(employee -> System.out.println(employee.age));
-        }
-    }
+
 }
--- src/impl.java
+++ src/impl.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class impl {
+import java.util.ArrayList;
+
+class Emplo {
+    public static void main(String[] args) {
+        employee e1 = new employee(101, "Ayush", 56000, 65);
+        employee e2 = new employee(101, "Monish vysa", 56000, 6);
+        employee e3 = new employee(101, "abhi", 56000, 12);
+        ArrayList<employee> List = new ArrayList<>();
+        List.add(e1);
+        List.add(e2);
+        List.add(e3);
+        List.stream().filter(date-> date.age>26).forEach(employee -> System.out.println(employee.age));
+    }
 }
